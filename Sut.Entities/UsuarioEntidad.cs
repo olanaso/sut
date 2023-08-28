2f66aef810436f9ddd9c24fab58fbe96a4b0902f
juanjimenezm <juanjimenezm@hotmail.com>
2023-08-27 23:57:15 -0500

    JJJM 27-08-2023

---
+++ Sut.Entities/UsuarioEntidad.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sut.Entities
+{
+    class UsuarioEntidad
+    {
+        public Entidad Entidad { get; set; }
+        public Usuario Usuario { get; set; }
+        public short Rol { get; set; }
+        public string LogoEntidad { get; set; }
+        public long EntidadId { get; set; }
+        public long UsuarioId { get; set; }
+
+    }
+}
